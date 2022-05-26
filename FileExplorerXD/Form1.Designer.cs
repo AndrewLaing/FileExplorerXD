34a949e3747b143d0d3c085247f795862fa65161
Andrew Laing <parisianconnections@gmail.com>
2022-05-26 12:58:40 +0100

    Add project files.

---
+++ FileExplorerXD/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace FileExplorerXD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dirsTreeView = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.path_textBox = new System.Windows.Forms.TextBox();
+            this.refresh_button = new System.Windows.Forms.Button();
+            this.up_button = new System.Windows.Forms.Button();
+            this.forward_button = new System.Windows.Forms.Button();
+            this.back_button = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.itemCountStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dirsTreeView
+            // 
+            this.dirsTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dirsTreeView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dirsTreeView.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dirsTreeView.HideSelection = false;
+            this.dirsTreeView.Location = new System.Drawing.Point(0, 48);
+            this.dirsTreeView.Name = "dirsTreeView";
+            this.dirsTreeView.ShowLines = false;
+            this.dirsTreeView.Size = new System.Drawing.Size(171, 496);
+            this.dirsTreeView.TabIndex = 0;
+            this.dirsTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.dirsTreeView_BeforeExpand);
+            this.dirsTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.dirsTreeView_AfterSelect);
+            this.dirsTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.dirsTreeView_NodeMouseDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.path_textBox);
+            this.panel1.Controls.Add(this.refresh_button);
+            this.panel1.Controls.Add(this.up_button);
+            this.panel1.Controls.Add(this.forward_button);
+            this.panel1.Controls.Add(this.back_button);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(654, 48);
+            this.panel1.TabIndex = 3;
+            // 
+            // path_textBox
+            // 
+            this.path_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.path_textBox.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.path_textBox.Location = new System.Drawing.Point(104, 10);
+            this.path_textBox.Name = "path_textBox";
+            this.path_textBox.Size = new System.Drawing.Size(504, 23);
+            this.path_textBox.TabIndex = 0;
+            this.path_textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.path_textBox_KeyDown);
+            // 
+            // refresh_button
+            // 
+            this.refresh_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.refresh_button.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh_button.Location = new System.Drawing.Point(616, 8);
+            this.refresh_button.Name = "refresh_button";
+            this.refresh_button.Size = new System.Drawing.Size(28, 28);
+            this.refresh_button.TabIndex = 4;
+            this.refresh_button.Text = "🗘";
+            this.refresh_button.UseVisualStyleBackColor = true;
+            this.refresh_button.Click += new System.EventHandler(this.refresh_button_Click);
+            // 
+            // up_button
+            // 
+            this.up_button.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.up_button.Location = new System.Drawing.Point(72, 8);
+            this.up_button.Name = "up_button";
+            this.up_button.Size = new System.Drawing.Size(28, 28);
+            this.up_button.TabIndex = 3;
+            this.up_button.Text = "ᐱ";
+            this.up_button.UseVisualStyleBackColor = true;
+            this.up_button.Click += new System.EventHandler(this.up_button_Click);
+            // 
+            // forward_button
+            // 
+            this.forward_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forward_button.Location = new System.Drawing.Point(40, 8);
+            this.forward_button.Name = "forward_button";
+            this.forward_button.Size = new System.Drawing.Size(28, 28);
+            this.forward_button.TabIndex = 2;
+            this.forward_button.Text = "＞";
+            this.forward_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.forward_button.UseVisualStyleBackColor = true;
+            this.forward_button.Click += new System.EventHandler(this.forward_button_Click);
+            // 
+            // back_button
+            // 
+            this.back_button.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.back_button.Location = new System.Drawing.Point(8, 8);
+            this.back_button.Name = "back_button";
+            this.back_button.Size = new System.Drawing.Size(28, 28);
+            this.back_button.TabIndex = 1;
+            this.back_button.Text = "＜";
+            this.back_button.UseVisualStyleBackColor = true;
+            this.back_button.Click += new System.EventHandler(this.back_button_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dirsTreeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(654, 542);
+            this.splitContainer1.SplitterDistance = 169;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 48);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(478, 496);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "unknown.png");
+            this.imageList1.Images.SetKeyName(1, "NoRootDirectory.png");
+            this.imageList1.Images.SetKeyName(2, "removable.png");
+            this.imageList1.Images.SetKeyName(3, "fixed.png");
+            this.imageList1.Images.SetKeyName(4, "network.png");
+            this.imageList1.Images.SetKeyName(5, "CDRom.png");
+            this.imageList1.Images.SetKeyName(6, "RAM.png");
+            this.imageList1.Images.SetKeyName(7, "locked.png");
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.Window;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemCountStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 542);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(654, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // itemCountStatusLabel
+            // 
+            this.itemCountStatusLabel.Name = "itemCountStatusLabel";
+            this.itemCountStatusLabel.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.itemCountStatusLabel.Size = new System.Drawing.Size(78, 17);
+            this.itemCountStatusLabel.Text = "FileCount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 564);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FileExplorerXD";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView dirsTreeView;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox path_textBox;
+        private System.Windows.Forms.Button refresh_button;
+        private System.Windows.Forms.Button up_button;
+        private System.Windows.Forms.Button forward_button;
+        private System.Windows.Forms.Button back_button;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel itemCountStatusLabel;
+    }
+}
+
---
+++ FileExplorerXD/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace FileExplorerXD
+{
+    /* DriveType Enum values used as Indexes for drive icons in imageList1
+     * -----------------------------------------------------------------------------------------
+     * Unknown          - 0 - The type of drive is unknown. 
+     * NoRootDirectory  - 1 - The drive does not have a root directory.
+     * Removable        - 2 - The drive is a removable storage device, such as a USB flash drive.
+     * Fixed            - 3 - The drive is a fixed disk.
+     * Network          - 4 - The drive is a network drive.
+     * CDRom            - 5 - The drive is an optical disc device, such as a CD or DVD-ROM.
+     * Ram              - 6 - The drive is a RAM disk.
+    */
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetStartDirectory();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            dirsTreeView.Focus();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CreateInitialTreeViewStructure();
+        }
+
+        private void SetStartDirectory()
+        {
+            string start_directory = Environment.ExpandEnvironmentVariables("%USERPROFILE%");
+            start_directory = start_directory.Replace(@"\", "/");
+            webBrowser1.Navigate(start_directory);
+            path_textBox.Text = start_directory;
+        }
+
+
+        #region "TreeView"
+
+        private void CreateInitialTreeViewStructure()
+        {
+            dirsTreeView.ImageList = imageList1; // drive icons
+            string[] drive_list = Environment.GetLogicalDrives();
+
+            foreach (string drive in drive_list)
+            {
+                DriveInfo di = new DriveInfo(drive);
+
+                // TEXT, ImageIDX, selectedImageIDX
+                TreeNode node = new TreeNode(drive,
+                                             (int)di.DriveType,
+                                             (int)di.DriveType);
+                node.Tag = drive;
+
+                if (di.IsReady == true)
+                {
+                    node.Nodes.Add("...");
+                }
+
+                dirsTreeView.Nodes.Add(node);
+            }
+            dirsTreeView.Update();
+            updateTreeViewWithSelection();
+        }
+
+        private void AddChildNodesToTreeNode(TreeNode tn)
+        {
+            if (tn.Nodes.Count <= 0) return;
+
+            if (tn.Nodes[0].Text == "..." && tn.Nodes[0].Tag == null)
+            {
+                tn.Nodes.Clear();
+
+                string[] subdirs = Directory.GetDirectories(tn.Tag.ToString());
+
+                foreach (string dir in subdirs)
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    TreeNode node = new TreeNode(di.Name,
+                                                (int)DriveType.Unknown,
+                                                (int)DriveType.NoRootDirectory);
+
+                    try
+                    {
+                        node.Tag = dir;
+                        if (di.GetDirectories().Count() > 0)
+                        {
+                            node.Nodes.Add(null, "...",
+                                                (int)DriveType.Unknown,
+                                                (int)DriveType.NoRootDirectory);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        node.ImageIndex = 7;          //display a locked folder icon
+                        node.SelectedImageIndex = 7;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "FileExplorerXD",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        tn.Nodes.Add(node);
+                    }
+                }
+            }
+        }
+
+        private void updateTreeViewWithSelection()
+        {
+            if (dirsTreeView.Nodes.Count <= 0) return;
+
+            TreeNodeCollection nodes = null;
+            TreeNode tn = null;
+            TreeNode found = null;
+
+            string dir;
+            string path = DecodeUrl(webBrowser1.Url.AbsolutePath);
+            string[] parts = path.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            dirsTreeView.CollapseAll();
+            nodes = dirsTreeView.Nodes;
+            dir = parts[0];
+
+            if (dir[dir.Length - 1] == ':')
+            {
+                dir += '\\';   // Drive nodes are named 'C:\' etc
+            }
+
+            // Find the drive node
+            for (int j = 0; j < nodes.Count; j++)
+            {
+                tn = nodes[j];
+                if (tn.Text == dir)
+                {
+                    found = tn;
+                    break;
+                }
+            }
+
+            AddChildNodesToTreeNode(found);
+
+            for (int i = 1; i < parts.Length; i++)
+            {
+                dir = parts[i];
+
+                for (int j = 0; j < found.Nodes.Count; j++)
+                {
+                    tn = found.Nodes[j];
+
+                    if (tn.Text == dir)
+                    {
+                        found = tn;
+                        AddChildNodesToTreeNode(found);
+                        break;
+                    }
+                }
+            }
+
+            dirsTreeView.SelectedNode = found;
+            dirsTreeView.SelectedNode.Expand();
+            dirsTreeView.SelectedNode.EnsureVisible();
+            dirsTreeView.Focus(); // focus to highlight selected node
+        }
+
+        private void dirsTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Collapse Other Nodes of TreeView on Selection of a Node
+            if (e.Node.Parent != null)
+            {
+                foreach (TreeNode node in e.Node.Parent.Nodes)
+                {
+                    if (node != e.Node)
+                        node.Collapse();
+                }
+            }
+        }
+
+        private void dirsTreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            AddChildNodesToTreeNode(e.Node);
+        }
+
+        private void dirsTreeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                dirsTreeView.SelectedNode = e.Node;
+                path_textBox.Text = dirsTreeView.SelectedNode.FullPath.ToString();
+                path_textBox.Text = path_textBox.Text.Replace(@"\\", @"\");
+                webBrowser1.Url = new Uri(path_textBox.Text);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                dirsTreeView.SelectedNode.ImageIndex = 7;    //display a locked folder icon
+                dirsTreeView.SelectedNode.SelectedImageIndex = 7;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "FileExplorerXD", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        #region "WebBrowserView"
+
+        private void back_button_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+            }
+            dirsTreeView.Focus();
+        }
+
+        private void forward_button_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+            dirsTreeView.Focus();
+        }
+
+        private void up_button_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(path_textBox.Text);
+
+            if (di.Parent != null)
+            {
+                string parent = di.Parent.FullName;
+                webBrowser1.Url = new Uri(parent);
+            }
+            dirsTreeView.Focus();
+        }
+
+        private void refresh_button_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+            dirsTreeView.Focus();
+        }
+
+        private void path_textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                string path = path_textBox.Text;
+
+                if (Directory.Exists(path))
+                {
+                    if (path[path.Length - 1] == ':')
+                    {
+                        path += '/'; // C: can exist but not as a valid Uri
+                    }
+                    webBrowser1.Url = new Uri(path);
+                }
+                else
+                {
+                    // this allows %temp% etc
+                    string urlToNavigate = Environment.ExpandEnvironmentVariables(path);
+                    if (Directory.Exists(urlToNavigate))
+                    {
+                        webBrowser1.Url = new Uri(urlToNavigate);
+                        path_textBox.Text = urlToNavigate;
+                    }
+                    else
+                    {
+                        string message = "Can't find '" + path + "' check the path and try again";
+                        MessageBox.Show(message, "File Explorer", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                // Now suppress the 'ding' sound
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            path_textBox.Text = DecodeUrl(webBrowser1.Url.AbsolutePath);
+            updateTreeViewWithSelection();
+            updateItemCountStatusLabel();
+        }
+
+        private string DecodeUrl(string url)
+        {
+            string decoded_Url;
+
+            while ((decoded_Url = Uri.UnescapeDataString(url)) != url)
+            {
+                url = decoded_Url;
+            }
+
+            return decoded_Url;
+        }
+
+
+        private void updateItemCountStatusLabel()
+        {
+            string path = DecodeUrl(webBrowser1.Url.AbsolutePath);
+
+            var directoryInfo = new System.IO.DirectoryInfo(path);
+            try
+            {
+                int item_count = directoryInfo.GetDirectories().Length + directoryInfo.GetFiles().Length;
+
+                if (item_count == 1)
+                {
+                    itemCountStatusLabel.Text = item_count + " item";
+                }
+                else
+                {
+                    itemCountStatusLabel.Text = item_count + " items";
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                dirsTreeView.SelectedNode.ImageIndex = 7;    //display a locked folder icon
+                dirsTreeView.SelectedNode.SelectedImageIndex = 7;
+            }
+        }
+
+
+        #endregion
+
+        // ///////////////////////////////////////////////////////////////////////////////
+        // //////////// IN PROGRESS //////////////////////////////////////////////////////
+        // ///////////////////////////////////////////////////////////////////////////////
+
+
+    }
+}
